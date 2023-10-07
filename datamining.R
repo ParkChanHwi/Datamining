1f37a90a767703afb7a9d23db4d1c1b815c803db
Parkchanhwi <56109340+ParkChanHwi@users.noreply.github.com>
2023-10-07 15:45:08 +0900

    Add files via upload

---
+++ datamining.R
@@ -0,0 +1,19 @@
+a = c(9,12,11,11,7,12,6,11,4,10,10,11,9,10,7,10,8,8,9,8,6,10,6,8,11)
+
+mean(a) #평균
+
+median(a) # 중앙값
+
+var(a) # 분산
+
+sd(a) # 표준편차
+
+range = max(a) - min(a) # 범위
+
+print(range)
+
+summary(a)
+
+
+
+edu = read.table("education.txt",header = TRUE)
